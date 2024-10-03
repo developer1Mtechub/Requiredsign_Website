ea1cde3ce5ea0838558bd0f0962aad504baba116
Developer1 Mtechub <developer1.mtechub@gmail.com>
2024-10-03 15:48:02 +0500

    Update url.jsx

--- src/url.jsx
+++ src/url.jsx
@@ -1,6 +1,6 @@
 const try_for_free = "https://portal.requiresign.com/register";
 const login = "https://portal.requiresign.com/login"
 
-const API_URL = "https://require-sign-be.caprover-demo.mtechub.com";
+const API_URL = "http://192.168.18.130:3006/";
 
 export { try_for_free, login, API_URL }
